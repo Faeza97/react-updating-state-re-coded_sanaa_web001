fed6884858803bb0351ff08ca833bd1bcda9a111
Faeza Mohammed <fayza.salman20@gmail.com>
2021-03-04 10:44:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LightSwitch from './components/LightSwitch';
+<<<<<<< HEAD
 import ClickityClick from './components/ClickityClick';
+=======
+
+>>>>>>> 832bd15dc503a08bae5c5818595114a13bc24105
  
 ReactDOM.render(<LightSwitch />, document.getElementById('root'));
